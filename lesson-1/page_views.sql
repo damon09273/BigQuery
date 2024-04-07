98fd75b0473c68219d22eb4f22ceb13289730156
Damon <damon09273@gmail.com>
2024-04-07 22:25:56 +0800

    Update page_views.sql

--- lesson-1/page_views.sql
+++ lesson-1/page_views.sql
@@ -1,13 +1,6 @@
-SELECT
-  event_name,
-  (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'page_location') AS page_location,
-  COUNT(*) AS page_views
-FROM
-  `doordata-course.ga4_sample.events_20210122`  -- 替換成你的專案 ID 和資料集 ID
-WHERE
-  event_name = 'page_view'  -- 假設 'page_view' 是網頁瀏覽事件的名稱
-GROUP BY
-  event_name,
-  page_location
-ORDER BY
-  page_views DESC;
+# 使用事件資料
+SELECT eps.value.string_value AS url, COUNT(*) AS pv
+FROM `doordata-course.ga4_sample.events_20210131`, UNNEST(event_params) AS eps
+WHERE key = 'page_location' AND event_name = 'page_view'
+GROUP BY 1
+ORDER BY 2 DESC
