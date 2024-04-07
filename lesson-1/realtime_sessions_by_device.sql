26008dee35ab60862bf1bf9cb607a0468b0bb682
Damon <damon09273@gmail.com>
2024-04-07 22:21:53 +0800

    Create realtime_sessions_by_device.sql

---
+++ lesson-1/realtime_sessions_by_device.sql
@@ -0,0 +1,12 @@
+-- 即時報表 by 裝置
+SELECT
+  device.category,
+  COUNT(*) AS devices
+FROM
+  -- 換成你的表格
+  `project.dataset.events_intraday_20240405`
+WHERE
+  event_name = 'session_start'
+  AND TIMESTAMP_MICROS(event_timestamp) >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 30 MINUTE)
+GROUP BY
+  device.category
